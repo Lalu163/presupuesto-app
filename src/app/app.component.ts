61360b4b80a1dce5b4014789b2df6988743e5deb
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-09-28 12:16:04 +0200

    funciones para determinar gastos e ingresos totales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Ingreso } from './ingreso/ingreso.model';
+import { Gasto } from './gasto/gasto.model';
+import { IngresoServicio } from './ingreso/ingreso.servicio';
+import { GastoServicio } from './gasto/gasto.servicio';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+  
+  ingresos:Ingreso[]=[];
+  gastos: Gasto[]=[];
+
+  constructor(private ingresoServicio:IngresoServicio,
+              private gastoServicio:GastoServicio){
+                this.ingresos = ingresoServicio.ingresos;
+                this.gastos = gastoServicio.gastos;
+              }
+  
+  getIngresoTotal(){
+    let ingresoTotal:number=0;
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal = ingresoTotal + ingreso.valor;
+    });
+    return ingresoTotal;
+  }
+  
+  getGastoTotal(){
+    let gastoTotal:number=0;
+    this.gastos.forEach(gasto => {
+      gastoTotal = gastoTotal + gasto.valor;
+    });
+    return gastoTotal;
+  }
+
+  getPorcentajeTotal(){
+    return.this.getGastoTotal()/this.getIngresoTotal();
+  }
+
+  getPresupuestoTotal(){
+    return this.getIngresoTotal()-this.getGastoTotal();
+  }
+
 }
